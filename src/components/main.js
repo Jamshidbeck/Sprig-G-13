5ad83cefde21ecc5a409c412beae3dd8b26d151a
Jamshidbeck <jamshidabdullajonov@gmail.com>
2022-07-12 19:38:14 +0500

    jhgkjbv

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../components/Main.css"
 import { Jamshid } from "../pages/Jamshid/Jamshid";
+import { Qoshimcha } from "../pages/Qoshimcha/Qoshimcha";
 
 export function Main(){
     return(
         <div className="main">
             <Jamshid/>
+            <Qoshimcha/>
         </div>
     )
 }
--- src/pages/Qoshimcha/qoshimcha.js
+++ src/pages/Qoshimcha/qoshimcha.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "../Qoshimcha/Qoshimcha.css"
 
-export function Boost(){
+export function Qoshimcha(){
     return(
-        <div></div>
+        <div className="boost">
+            <div className="yellow-page">
+                <div className="yellow-page-left"></div>
+                <div className="yellow-page-right"></div>    
+            </div>
+            <div className="black-page">
+<p>salom</p>
+            </div>
+        </div>
     )
 }
