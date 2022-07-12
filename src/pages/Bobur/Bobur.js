598e127fbfd340defbfc59baeeb2521a4d7ea505
Boburbek0101 <boburolimjonov0121@gmail.com>
2022-07-12 20:06:51 +0500

    sajagjsgha

--- src/pages/Bobur/Bobur.js
+++ src/pages/Bobur/Bobur.js
@@ -1,7 +1,35 @@
 import React from "react";
-
+import "./bobur.css"
+import photo from "./img/IMAGE.png"
 export function Bobur(){
     return(
-        <div></div>
+        <div className="Bobur">
+            <div className="section1">
+                <div className="sectionL">
+
+                    
+                   <div className="section_left_text">
+                   <p>In-context user research that's fast and reliable.</p>
+                   <span>Ensure user research happens early and often with Sprig's targeted microsurveys, video questions, and more.</span>
+
+
+                   <div className="sec_1_btn">
+                    <button>Start for Free</button>
+                    <button>Talk to Sales</button>
+                   </div>
+                   </div>
+    
+
+                </div>
+
+
+                <div className="sectionR">
+                    <img id="img" src={photo} alt="" />
+                </div>
+
+            
+            </div>
+           
+        </div>
     )
 }
